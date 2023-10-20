4062e3314e183b92cae51a02410dbb6ef1c0f83d
Sachin Kant <sachinkant404@gmail.com>
2023-10-21 02:31:14 +0530

    implemented ModelViewSet for all the Models

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from gymapp.views import GymViewSet
+from gymapp.views import GymViewSet, TrainerViewSet, ClientViewSet, WorkoutSessionViewSet
 
 router = DefaultRouter()
 
 router.register(r'gyms', GymViewSet, basename='gym')
+router.register(r'trainers', TrainerViewSet, basename='trainer')
+router.register(r'clients', ClientViewSet, basename='client')
+router.register(r'workoutsessions', WorkoutSessionViewSet, basename='workoutsession')
 
 urlpatterns = [
     path('', include(router.urls)),
--- gymapp/views.py
+++ gymapp/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 from .models import Gym, Trainer, Client, WorkoutSession
 from .serializers import GymSerializer, TrainerSerializer, ClientSerializer, WorkoutSessionSerializer
 
@@ -7,4 +6,16 @@ from .serializers import GymSerializer, TrainerSerializer, ClientSerializer, Wor
 class GymViewSet(viewsets.ModelViewSet):
     queryset = Gym.objects.all()
     serializer_class = GymSerializer
+
+class TrainerViewSet(viewsets.ModelViewSet):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class WorkoutSessionViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
 
