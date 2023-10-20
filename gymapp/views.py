726bac3fe4252a881043f49fa7d4f75a241a9949
Sachin Kant <sachinkant404@gmail.com>
2023-10-21 03:44:28 +0530

    implemented token based authentication

--- gymapp/views.py
+++ gymapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Gym, Trainer, Client, WorkoutSession
 from .serializers import GymSerializer, TrainerSerializer, ClientSerializer, WorkoutSessionSerializer
+from .auth import IsTrainerOrReadOnly
 
 
 class GymViewSet(viewsets.ModelViewSet):
@@ -18,4 +19,6 @@ class ClientViewSet(viewsets.ModelViewSet):
 class WorkoutSessionViewSet(viewsets.ModelViewSet):
     queryset = WorkoutSession.objects.all()
     serializer_class = WorkoutSessionSerializer
+    # check if the user is a trainer or it's a safe (read-only) request
+    permission_classes = [IsTrainerOrReadOnly]
 
