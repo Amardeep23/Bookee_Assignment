1c4a7c050e5729f469c0b74ad26050a1d96f205c
Sachin Kant <sachinkant404@gmail.com>
2023-10-21 04:00:39 +0530

    implemented error handling

--- gymapp/serializers.py
+++ gymapp/serializers.py
@@ -15,6 +15,11 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['id', 'name', 'age']
+    
+    def validate_age(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Age must be a positive integer.")
+        return value
 
 class WorkoutSessionSerializer(serializers.ModelSerializer):
     class Meta:
